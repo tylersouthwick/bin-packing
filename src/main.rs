85fe6c06f42f46edd3d4475a9d5405d255cc64b6
Tyler Southwick <tyler@northfuse.net>
2016-04-19 22:32:20 -0700

    implement packing

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ extern crate clap;
 
 use clap::{Arg, App};
 
-use std::iter::FromIterator;
 use std::io::Error;
 use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
     let matches = App::new("bin-packer")
                           .version("1.0")
                           .author("Tyler Southwick <tyler@northfuse.net>")
@@ -26,7 +24,7 @@ fn main() {
                                .index(1))
                           .get_matches();
 
-    let max = matches.value_of("binSize").unwrap();
+    let max = matches.value_of("binSize").unwrap().parse::<u32>().unwrap();
 
     let size_file = matches.value_of("sizes").unwrap();
 
@@ -35,19 +33,20 @@ fn main() {
     let lines = process_file(size_file).unwrap();
 
     println!("found {} lines", lines.len());
-    for x in lines {
-        println!("line: {}", x);
-    }
+
+    let bins = bin_pack(max, lines);
+
+    println!("bins={}", bins);
 }
 
 fn process_file(file_name : &str) -> Result<Vec<u32>, Error> {
     let mut f = try!(File::open(file_name));
     let mut contents = String::new();
     try!(f.read_to_string(&mut contents));
-    let rawList = contents.split("\n")
+    let raw_list = contents.split("\n")
             .map(|s : &str| match s.parse::<u32>() {
                 Ok(n) => n,
-                Err(e) => 0
+                Err(_) => 0
             })
             .collect::<Vec<u32>>()
             .iter()
@@ -55,6 +54,56 @@ fn process_file(file_name : &str) -> Result<Vec<u32>, Error> {
             .map(|&x| x)
             .collect::<Vec<u32>>();
 
-    Ok(rawList)
+    Ok(raw_list)
 }
 
+fn bin_pack(max : u32, sizes : Vec<u32>) -> u32 {
+    println!("bin packing {} elements into size {}", sizes.len(), max);
+
+    let mut new_sizes : Vec<u32>= Vec::new();
+    for size in sizes {
+        split_num(max, size, &mut new_sizes);
+    }
+    new_sizes.sort_by(|x, y| y.cmp(x));
+
+    let bins = new_sizes
+        .iter()
+        //.fold((0, 0), |result, item| (result.0 + 1, *item));
+        //.fold((0, 0), |result, item| (result.0, result.1 + *item));
+        .fold((0, 0), |result, item| {
+              if result.1 + item > max {
+                  (result.0 + 1, *item)
+              } else {
+                  (result.0, result.1 + *item)
+              }
+        });
+        //.1;
+
+    println!("bins.0={} bins.1={}", bins.0, bins.1);
+
+    if bins.1 > 0 {
+        bins.0 + 1
+    } else {
+        bins.0
+    }
+}
+
+fn split_num(max : u32, num : u32, vec : &mut Vec<u32>) {
+    let mut x = num;
+    //println!("\nstarting num={}", num);
+    while x > 0 {
+        let y = min(max, x);
+        //println!("num={} x={} y={}", num, x, y);
+        x = x - y;
+        vec.push(y);
+    }
+    //println!("ending num={}\n", num);
+}
+
+fn min(a : u32, b : u32) -> u32 {
+    if a < b {
+        a
+    } else {
+        b
+    }
+}
